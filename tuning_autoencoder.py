6a926e83ff842c389e5797dd41fc7dfbe7edebb9
Ichida <alexandre.ichida@lojasrenner.com.br>
2022-09-29 17:06:41 -0300

    tuning

--- tuning_autoencoder.py
+++ tuning_autoencoder.py
@@ -3,14 +3,14 @@ import pandas as pd
 import itertools
 
 if __name__ == '__main__':
-    all_emb_dims = [50, 100, 150]
-    all_h_dims = [50, 100, 150]
+    all_emb_dims = [100, 200, 300]
+    all_h_dims = [100, 200, 300]
     all_latent_dims = [15, 30]
 
     all_results = []
-    for emb_dim, h_dim, latent_dim in itertools.product(all_latent_dims, all_h_dims, all_latent_dims):
+    for emb_dim, h_dim, latent_dim in itertools.product(all_emb_dims, all_h_dims, all_latent_dims):
         result_df = train(emb_dim=emb_dim, h_dim=h_dim, latent_dim=latent_dim, epochs=50)
         all_results.append(result_df)
 
     tunning_df = pd.concat(all_results)
-    tunning_df.to_csv("train_results/tunning_results_old.csv", index=False)
+    tunning_df.to_csv("train_results/tunning_results.csv", index=False)
