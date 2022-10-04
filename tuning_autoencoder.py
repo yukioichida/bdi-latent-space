152a33d2afed0dbee7541531b2e62b2868ba5cf2
Ichida <ichida@DESKTOP-0FDT588.localdomain>
2022-10-04 07:32:23 -0300

    fix_rnn_bug

--- tuning_autoencoder.py
+++ tuning_autoencoder.py
@@ -1,12 +1,15 @@
 from train import train
 import pandas as pd
 import itertools
+import shortuuid
 
 if __name__ == '__main__':
     all_emb_dims = [100, 200, 300]
     all_h_dims = [100, 200, 300]
     all_latent_dims = [15, 30]
     all_activations = ['gumbel', 'bc']
+
+    tunning_id = shortuuid.ShortUUID().random(length=8)
 
     all_results = []
     i = 0
@@ -19,4 +22,4 @@ if __name__ == '__main__':
         i += 1
 
     tunning_df = pd.concat(all_results)
-    tunning_df.to_csv("train_results/tunning_results.csv", index=False)
+    tunning_df.to_csv(f"train_results/tunning_results_{tunning_id}.csv", index=False)
