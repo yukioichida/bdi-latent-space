97e74e2c4d461f7d4ae58d9dd5fcb7cc6bbbb0eb
Ichida <ichida@DESKTOP-0FDT588.localdomain>
2022-10-04 07:31:30 -0300

    fix_rnn_bug

--- tuning_autoencoder.py
+++ tuning_autoencoder.py
@@ -6,12 +6,15 @@ if __name__ == '__main__':
     all_emb_dims = [100, 200, 300]
     all_h_dims = [100, 200, 300]
     all_latent_dims = [15, 30]
+    all_activations = ['gumbel', 'bc']
 
     all_results = []
     i = 0
-    for emb_dim, h_dim, latent_dim in itertools.product(all_emb_dims, all_h_dims, all_latent_dims):
+    for emb_dim, h_dim, latent_dim, activation in itertools.product(all_emb_dims, all_h_dims, all_latent_dims,
+                                                                    all_activations):
         train_id = f'tunning_{i}'
-        result_df = train(train_id=train_id, emb_dim=emb_dim, h_dim=h_dim, latent_dim=latent_dim, epochs=50)
+        result_df = train(train_id=train_id, emb_dim=emb_dim, h_dim=h_dim, latent_dim=latent_dim, epochs=50,
+                          activation=activation)
         all_results.append(result_df)
         i += 1
 
